bdf3b6e77912e62b43f51477008c0ea3bca618a3
AnYuan <donganyuan1010@gmail.com>
2021-05-09 17:09:59 +0800

    refactor: hand_rank

--- unit1/poker.py
+++ unit1/poker.py
@@ -66,16 +66,10 @@ def hand_rank(hand):
         ranks = (5, 4, 3, 2, 1)
     straight = len(ranks) == 5 and max(ranks)-min(ranks) == 4
     flush = len(set([s for r, s in hand])) == 1
-    return (9 if (5,) == counts else
-            8 if straight and flush else
-            7 if (4, 1) == counts else
-            6 if (3, 2) == counts else
-            5 if flush else
-            4 if straight else
-            3 if (3, 1, 1) == counts else
-            2 if (2, 2, 1) == counts else
-            1 if (2, 1, 1, 1) == counts else
-            0), ranks
+    return max(count_rankings[counts], 4*straight + 5*flush), ranks
+
+
+count_rankings = {(5,): 10, (4, 1): 7, (3, 2): 6, (3, 1, 1): 3, (2, 2, 1): 2, (2, 1, 1, 1): 1, (1, 1, 1, 1, 1): 0}
 
 
 def group(items):
