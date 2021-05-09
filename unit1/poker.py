935f999e55fc3bdda71dabe5a1e75640b8238941
AnYuan <donganyuan1010@gmail.com>
2021-05-09 14:50:49 +0800

    add kind and two_pair

--- unit1/poker.py
+++ unit1/poker.py
@@ -21,6 +21,28 @@ def flush(hand):
     return len(set(suits)) == 1
 
 
+def kind(n, ranks):
+    """Return the first rank that this hand has exactly n of.
+    Return None if there is no n-of-a-kind in the hand.
+    """
+    for r in ranks:
+        if ranks.count(r) == n:
+            return r
+    return None
+
+
+def two_pair(ranks):
+    """If there are two pair, return the two ranks as a
+    typle: (highest, lowest); otherwise reutrn None.
+    """
+    pair = kind(2, ranks)
+    lowpair = kind(2, list(reversed(ranks)))
+    if pair and lowpair != pair:
+        return (pair, lowpair)
+    else:
+        return None
+
+
 def hand_rank(hand):
     "Return a value ndicating the ranking of a hand."
     ranks = card_ranks(hand)
@@ -49,8 +71,17 @@ def test():
     sf = "6C 7C 8C 9C TC".split()
     fk = "9D 9H 9S 9C 7D".split()
     fh = "TD TC TH 7C 7D".split()
+    tp = "5S 5D 9H 9C 6S".split()
+    fkranks = card_ranks(fk)
+    tpranks = card_ranks(tp)
+    assert kind(4, fkranks) == 9
+    assert kind(3, fkranks) == None
+    assert kind(2, fkranks) == None
+    assert kind(1, fkranks) == 7
+    assert two_pair(fkranks) == None
+    assert two_pair(tpranks) == (9, 5)
     assert straight([9, 8, 7, 6, 5]) == True
-    assert straight([9, 8, 7, 6, 5]) == False
+    assert straight([9, 8, 8, 6, 5]) == False
     assert flush(sf) == True
     assert flush(fk) == False
     assert card_ranks(sf) == [10, 9, 8, 7, 6]
