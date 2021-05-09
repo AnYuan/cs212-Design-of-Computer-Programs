8ceb7a8cc07984e2749b8beae7f226cf72042201
AnYuan <donganyuan1010@gmail.com>
2021-05-09 14:25:46 +0800

    add card_ranks

--- unit1/poker.py
+++ unit1/poker.py
@@ -3,6 +3,13 @@ def poker(hands):
     return max(hands, key=hand_rank)
 
 
+def card_ranks(hand):
+    "Return a list of the ranks, sorted with higher first."
+    ranks = ['--23456789TJQKA'.index(r) for r, s in hand]
+    ranks.sort(reverse=True)
+    return ranks
+
+
 def hand_rank(hand):
     "Return a value ndicating the ranking of a hand."
     ranks = card_ranks(hand)
@@ -31,6 +38,9 @@ def test():
     sf = "6C 7C 8C 9C TC".split()
     fk = "9D 9H 9S 9C 7D".split()
     fh = "TD TC TH 7C 7D".split()
+    assert card_ranks(sf) == [10, 9, 8, 7, 6]
+    assert card_ranks(fk) == [9, 9, 9, 9, 7]
+    assert card_ranks(fh) == [10, 10, 10, 7, 7]
     assert poker([sf, fk, fh]) == sf
     assert poker([fk, fh]) == fk
     assert poker([fh, fh]) == fh
