1e4357d775aab8969584d42c7adb12bc0c91baf1
AnYuan <donganyuan1010@gmail.com>
2021-05-15 17:16:02 +0800

    update timedcalls

--- unit2/Zebra-Puzzle.py
+++ unit2/Zebra-Puzzle.py
@@ -37,7 +37,7 @@ def zebra_puzzle():
                 )
 
 
-def timecall(fn, *args):
+def timedcall(fn, *args):
     "Call function with args; return the time in seconds and return result."
     t0 = time.time()
     result = fn(*args)
@@ -45,4 +45,20 @@ def timecall(fn, *args):
     return t1 - t0, result
 
 
-print(timecall(zebra_puzzle))
+def timedcalls(n, fn, *args):
+    "Call function n times with args; return the min, avg, and max time"
+    if isinstance(n, int):
+        times = [timedcall(fn, *args)[0] for _ in range(n)]
+    else:
+        times = []
+        while sum(times) < n:
+            times.append(timedcall(fn, *args)[0])
+    return min(times), average(times), max(times)
+
+
+def average(numbers):
+    "Return the average (arithmetic mean) of a sequence of numbers."
+    return sum(numbers) / float(len(numbers))
+
+
+print(timedcall(zebra_puzzle))
