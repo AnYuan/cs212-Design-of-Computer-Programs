dce7844f14a3668bb4ca5672d0ee11bba8af32bf
AnYuan <donganyuan1010@gmail.com>
2021-05-15 17:43:51 +0800

    add count tool - aspect-oriented programming

--- unit2/Zebra-Puzzle.py
+++ unit2/Zebra-Puzzle.py
@@ -1,3 +1,4 @@
+from os import statvfs_result
 import time
 import itertools
 
@@ -15,26 +16,45 @@ def zebra_puzzle():
     houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
     orderings = list(itertools.permutations(houses))  # 1
     return next((WATER, ZEBRA)
-                for (red, green, ivory, yellow, blue) in orderings
+                for (red, green, ivory, yellow, blue) in c(orderings)
                 if imright(green, ivory)  # 6
-                for (Englishman, Spaniard, Ukranian, Japanese, Norwegian) in orderings
+                for (Englishman, Spaniard, Ukranian, Japanese, Norwegian) in c(orderings)
                 if Englishman is red  # 2
                 if Norwegian is first  # 10
                 if nextto(Norwegian, blue)  # 15
-                for (coffee, tea, milk, oj, WATER) in orderings
+                for (coffee, tea, milk, oj, WATER) in c(orderings)
                 if coffee is green  # 4
                 if Ukranian is tea  # 5
                 if milk is middle  # 9
-                for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
+                for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in c(orderings)
                 if Kools is yellow  # 8
                 if LuckyStrike is oj  # 13
                 if Japanese is Parliaments  # 14
-                for (dog, snails, fox, horse, ZEBRA) in orderings
+                for (dog, snails, fox, horse, ZEBRA) in c(orderings)
                 if Spaniard is dog  # 3
                 if OldGold is snails  # 7
                 if nextto(Chesterfields, fox)  # 11
                 if nextto(Kools, horse)  # 12
                 )
+
+
+def c(sequence):
+    """
+    Generate items in sequence; keeping counts as we go. 
+    c.starts is the number of sequences started;
+    c.items is number of items generated.
+    """
+    c.starts += 1
+    for item in sequence:
+        c.items += 1
+        yield item
+
+
+def instrument_fn(fn, *args):
+    c.starts, c.items = 0, 0
+    result = fn(*args)
+    print('%s got %s with %5d iters over %7d items',
+          fn.__name__, result, c.starts, c.items)
 
 
 def timedcall(fn, *args):
@@ -61,4 +81,16 @@ def average(numbers):
     return sum(numbers) / float(len(numbers))
 
 
-print(timedcall(zebra_puzzle))
+def ints(start, end=None):
+    i = start
+    while i <= end or end is None:
+        yield i
+        i = i + 1
+
+
+def all_ints():
+    "Generate integers in the order 0, +1, -1, +2, -2, +3, -3, ..."
+    yield 0
+    for i in ints(1):
+        yield +i
+        yield -i
