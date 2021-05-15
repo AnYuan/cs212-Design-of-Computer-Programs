6e089b121f76014900b3e09faed1833c69f0746b
AnYuan <donganyuan1010@gmail.com>
2021-05-15 19:46:47 +0800

    add test

--- unit2/cryptarithmetic.py
+++ unit2/cryptarithmetic.py
@@ -1,6 +1,7 @@
 import string
 import re
 import itertools
+import time
 
 
 def valid(f):
@@ -24,5 +25,41 @@ def fill_in(formula):
     "Generate all possible fillings-in of letters in formula with digits."
     letters = ''.join(set(re.findall(r'[A-Z]', formula)))  # should be a string
     for digits in itertools.permutations('1234567890', len(letters)):
-        table = string.maketrans(letters, ''.join(digits))
+        table = str.maketrans(letters, ''.join(digits))
         yield formula.translate(table)
+
+
+def timedcall(fn, *args):
+    "Call function with args; return the time in seconds and return result."
+    t0 = time.time()
+    result = fn(*args)
+    t1 = time.time()
+    return t1 - t0, result
+
+
+examples = """TWO + TWO == FOUR
+A**2 + B**2 == C**2
+A**2 + BE**2 == BY**2
+X / X == X
+A**N + B**N == C**N and N > 1
+ATOM**0.5 == A + TO + M
+GLITTERS is not GOLD
+ONE < TWO and FOUR < FIVE
+ONE < TWO < THREE
+RAMN == R**3 + RM**3 == N**3 + RX**3
+sum(range(AA)) == BB
+sum(range(POP)) == BOBO
+ODD + ODD == EVEN
+PLUTO not in set([PLANETS])
+""".splitlines()
+
+
+def test():
+    t0 = time.time()
+    for example in examples:
+        print(13*' ', example)
+        print('%6.4f sec: %s ' % timedcall(solve, example))
+    print('%6.4f tot.' % (time.time()-t0))
+
+
+test()
