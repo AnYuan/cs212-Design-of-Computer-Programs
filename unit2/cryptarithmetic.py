faae97d541ab4065f1afc475c5abf853f8bd573a
AnYuan <donganyuan1010@gmail.com>
2021-05-15 20:34:04 +0800

    speed up

--- unit2/cryptarithmetic.py
+++ unit2/cryptarithmetic.py
@@ -41,6 +41,34 @@ def compile_word(word):
         return word
 
 
+def faster_solve(formula):
+    """Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
+    Input formula is a string; output is a digit-filled-in string or None.
+    This version precompiles the formula; only one eval per formula."""
+    f, letters = compile_formula(formula)
+    for digits in itertools.permutations((1, 2, 3, 4, 5, 6, 7, 8, 9, 0), len(letters)):
+        try:
+            if f(*digits) is True:
+                table = str.maketrans(letters, ''.join(map(str, digits)))
+                return formula.translate(table)
+        except ArithmeticError:
+            pass
+
+
+def compile_formula(formula, verbose=False):
+    """Compile formula into a function. Also return letters found, as a str, 
+    in same order as parms of function. For example, 'YOU == ME ** 2' returns
+    (lambda Y, M, E, U, O: (U+10*0+100*Y) == (E+10*M)**2), 'YMEUO' """
+    letters = ''.join(set(re.findall('[A-Z]', formula)))
+    parms = ', '.join(letters)
+    tokens = map(compile_word, re.split('([A-Z]+)', formula))
+    body = ''.join(tokens)
+    f = 'lambda %s: %s' % (parms, body)
+    if verbose:
+        print(f)
+        return eval(f), letters
+
+
 def timedcall(fn, *args):
     "Call function with args; return the time in seconds and return result."
     t0 = time.time()
